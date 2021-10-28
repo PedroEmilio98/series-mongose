50d84a4b02d8de9f6d790d72162298fdde72590f
Pedro Emilio <pedroemiliokuhn@gmail.com>
2021-10-28 15:03:45 -0300

    problema dos assets resolvido e testes removidos

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const pages = require('./routes/pages');
 const series = require('./routes/series');
-const teste = require('./routes/teste');
+
 
 //requisicoes para o banco de dados
 const mongodb = process.env.MONGO;
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+
+//assets
+app.use(express.static('public'));
 
 //view engine: EJS
 app.set('views', path.join(__dirname, 'views'));
@@ -22,13 +25,12 @@ app.set('view engine', 'ejs');
 //processar as requisicoes POST
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//assets
-app.use(express.static('public'));
+
 
 //faz o direcionamento das requisicoes para as rotas usando a primeira / 
 app.use('/', pages);
 app.use('/series', series);
-app.use('/teste', teste);
+
 
 //conexao com o mongo DB utilizando ATLAS
 mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
